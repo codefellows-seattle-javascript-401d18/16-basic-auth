e6ff1520791c7a29ddadecac2f0262b7277c9fb4
Aaron McPeek <arn1313@comcast.net>
2017-09-11 17:08:58 -0700

    updated server.js

--- lab-aaron/lib/server.js
+++ lab-aaron/lib/server.js
@@ -1,28 +1,53 @@
 'use strict';
 
 const debug = require('debug')('http:server');
-require('dotenv');
 
 // setting up express
 const express = require('express');
 const router = express.Router();
 const app = express();
-debug('shut up debug');
 
 // mongoose setup
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+
+let wat = mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 // router middleware
-require('../route/routh-auth');
+require('../route/routh-auth')(router);
 
 // mount middleware
 app.use(require('bodyParser').json());
 app.use(require('cors')());
 app.use(router);
 
-
 app.all('/*', (req, res) => res.sendStatus(404));
 
-module.exports = app;
+const server = module.exports = {};
+server.isOn = false;
+server.start = () => {
+  return new Promise((resolve, reject) => {
+    if(!server || !server.isOn) {
+      server.http = app.listen(process.env.PORT, () => {
+        debug(`Liistening on ${process.env.PORT}`);
+        server.isOn = true;
+        resolve();
+      });
+      return;
+    }
+    reject(new Error('server already running'));
+  });
+};
+
+server.stop = () => {
+  return new Promise((resolve, reject) => {
+    if(server.http && server.isOn) {
+      return server.http.close(() => {
+        wat.close();
+        server.isOn = false;
+        resolve();
+      });
+    }
+    reject(new Error('the server is not running'));
+  });
+};
